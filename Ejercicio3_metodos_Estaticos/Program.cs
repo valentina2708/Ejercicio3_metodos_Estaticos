190823aff54a60f3fc4a00c2b3434aa97a0c8fbf
valentina2708 <75132700+valentina2708@users.noreply.github.com>
2022-03-16 22:02:15 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio3_metodos_Estaticos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3_metodos_Estaticos
+{
+    internal class Program
+
+    {
+      static  int x = 12;
+       static int y = 5;
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("\nBase x: " + x + "\nAltura y: " +y);
+
+            resultado(x, y);
+
+            Console.ReadKey();
+        }
+
+        static int areaCuadrado (int bas, int altura)
+        {
+
+            return bas * altura;
+;       
+        }
+        static void resultado(int bas, int altura)
+        {
+            Console.WriteLine("Area del Cuadrado: " +areaCuadrado(bas, altura));
+        }
+
+    
+    }
+    
+}
